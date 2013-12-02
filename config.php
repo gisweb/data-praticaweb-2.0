856f073f99f23e96873c1c3d8f132d6c101988a6
mamogmx <marco.carbone@gmx.com>
2013-12-02 12:01:33 +0100

    a

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?
-define('NOME_COMUNE','Comune di Sanremo - Pratiche Edilizie');//nome completo del comune che compare nell'intestazione
+define('NOME_COMUNE','Comune di Andora - Pratiche Edilizie');//nome completo del comune che compare nell'intestazione
 
 define('DEBUG', 1); // Debugging 0 off 1 on
 define('DB_DRIVER','pdo_pgsql');
 define('DB_HOST','127.0.0.1');
-define('DB_PORT','5432');
-define('DB_NAME','');
+define('DB_PORT','5434');
+define('DB_NAME','gw_andora');
 define('DB_USER','postgres');
 define('DB_PWD','postgres');
 
