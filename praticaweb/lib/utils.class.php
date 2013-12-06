a3150a1e2918736a0f3c842351c35960faf9a25c
mamogmx <marco.carbone@gmx.com>
2013-12-06 12:13:02 +0100

    import files

--- praticaweb/lib/utils.class.php
+++ praticaweb/lib/utils.class.php
@@ -11,6 +11,11 @@
  * @author marco
  */
 class utils {
+    const jsURL = "/js";
+    const cssURL="/css";
+    public static $js = Array('jquery-1.9.1','jquery-ui-1.10.2.min','jquery.ui.datepicker-it','jquery.dataTables.min','dataTables.date.order','window','praticaweb','page.controller');
+    public static $css = Array('praticaweb/jquery-ui-1.9.1.custom','styles','TableTools','TableTools_JUI','demo_page','demo_table_jui','tabella_v','menu');
+    
     static function mergeParams($prms=Array(),$defaultParams=Array()){
         foreach($defaultParams as $key=>$val){
             $result[$key]=(!array_key_exists($key, $prms) || is_null($prms[$key]))?($val):($prms[$key]);
@@ -37,6 +42,19 @@ class utils {
 		$conn = new PDO($dsn, DB_USER, DB_PWD);
         return $conn;
     }
+    static function writeJS(){
+        foreach(self::$js as $js){
+           $tag=sprintf("\n\t\t<SCRIPT language=\"javascript\" src=\"%s/%s.js\"></script>",self::jsURL,$js);
+           echo $tag;
+        }
+        
+    }
+    static function writeCSS(){
+        foreach(self::$css as $css){
+           $tag=sprintf("\n\t\t<LINK media=\"screen\" href=\"%s/%s.css\" type=\"text/css\" rel=\"stylesheet\"></link>",self::cssURL,$css);
+           echo $tag;
+        }
+        
+    }
 }
-
 ?>
