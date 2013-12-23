c731cb9a447cb15029b33c76ee0ab03a235869ee
mamogmx <marco.carbone@gmx.com>
2013-12-23 17:25:56 +0100

    some improvement

--- praticaweb/lib/utils.class.php
+++ praticaweb/lib/utils.class.php
@@ -1,7 +1,15 @@
 <?php
-use Doctrine\Common\ClassLoader;
-require_once APPS_DIR.'plugins/Doctrine/Common/ClassLoader.php';
 
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of utils
+ *
+ * @author marco
+ */
 class utils {
     const jsURL = "/js";
     const cssURL="/css";
@@ -29,25 +37,10 @@ class utils {
     static function resizeImages($prms=Array()) {
         
     }
-    static function getPDODb($params=Array()){
+    static function getDb($params=Array()){
         $dsn = sprintf('pgsql:dbname=%s;host=%s;port=%s',DB_NAME,DB_HOST,DB_PORT);
 		$conn = new PDO($dsn, DB_USER, DB_PWD);
         return $conn;
-    }
-    static function getDoctrineDB($params=Array()){
-        $classLoader = new ClassLoader('Doctrine', APPS_DIR.'plugins/');
-		$classLoader->register();
-		$config = new \Doctrine\DBAL\Configuration();
-		$connectionParams = array(
-			'dbname' => DB_NAME,
-			'user' => DB_USER,
-			'password' => DB_PWD,
-			'host' => DB_HOST,
-			'port' => DB_PORT,
-			'driver' => DB_DRIVER,
-		);
-		$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
-		return $conn;
     }
     static function writeJS(){
         foreach(self::$js as $js){
@@ -82,6 +75,101 @@ class utils {
 
         // Return the string
         return $string;
+    }
+    static function debug($file,$data,$mode='a+'){
+	$f=fopen(DEBUG_DIR.$file.".debug",$mode);
+	ob_start();
+	print_r($data);
+	$result=ob_get_contents();
+	ob_end_clean();
+	fwrite($f,$result."\n");
+	fclose($f);
+    }
+    /*funzione di Raggruppamento dei dati*/
+    static function groupData($mode,$res){
+        $result=Array();
+        switch($mode){
+            case "civico":
+                for($i=0;$i<count($res);$i++){
+                    $rec=$res[$i];
+                    $codvia=preg_replace("/[^A-Za-z0-9]/", '', strtolower($rec["via"]));
+                    $via=$rec["via"];
+                    $civico=preg_replace("([\\/]+)","-",$rec["civico"]);
+                    $civico=preg_replace("([\.]+)","",$civico);
+                    $descrizione=sprintf("Pratica n° %s del %s",$rec["numero"],$rec["data_presentazione"]);
+                    $ct=$rec["elenco_ct"];
+                    $cu=$rec["elenco_cu"];
+                    $linkToPratica=$rec["pratica"];
+                    $r[$codvia][$civico][$rec["pratica"]]=Array("via"=>$via,"civico"=>$civico,"info"=>Array("id"=>$rec["pratica"],"name"=>$descrizione,"descrizione"=>$descrizione,"ct"=>$ct,"cu"=>$cu,"civico"=>"","via"=>"","pratica"=>$linkToPratica,"oggetto"=>$rec["oggetto"]));
+
+                }
+                foreach($r as $codvia=>$values){
+                    $civici=Array();
+                    foreach($values as $civ=>$vals){
+                        $pratiche=Array();
+                        foreach($vals as $pr=>$data){
+                            $pratiche[]=$data["info"];
+                            $via=$data["via"];
+                        }
+                        $civico=Array("id"=>"$codvia-$civ","name"=>$civ,"civico"=>"$civ","via"=>"$via","descrizione"=>"","ct"=>"","cu"=>"","children"=>$pratiche,"oggetto"=>"","pratica"=>"","state"=>"closed");
+                        $civici[]=$civico;
+                    }
+                    $via=Array("id"=>"$codvia","civico"=>"","name"=>$via,"via"=>"$via","oggetto"=>"","descrizione"=>"","ct"=>"","cu"=>"","pratica"=>"","state"=>"closed","children"=>$civici);
+                    $result[]=$via;
+                }
+
+                break;
+            case "particella-terreni":
+            case "particella-urbano":
+                for($i=0;$i<count($res);$i++){
+                    $rec=$res[$i];
+                    $sez=preg_replace("/[^A-Za-z0-9 ]/", '', strtolower($rec["sezione"]));
+                    $fg=preg_replace("/[^A-Za-z0-9 ]/", '',$rec["foglio"]);
+                    $mp=preg_replace("/[^A-Za-z0-9 ]/", '',$rec["mappale"]);
+                    $descrizione=sprintf("Pratica n° %s del %s",$rec["numero"],$rec["data_presentazione"]);
+                    $ubicazione=$rec["ubicazione"];
+                    $cu=$rec["elenco_cu"];
+                    $r[$sez][$fg][$mp][$rec["pratica"]]=Array(
+                        "sezione"=>$sez,
+                        "foglio"=>$fg,
+                        "mappale"=>$mp,
+                        "info"=>Array(
+                            "id"=>$rec["pratica"],
+                            "name"=>$descrizione,
+                            "descrizione"=>$descrizione,
+                            "ubicazione"=>$ubicazione,
+                            "cu"=>$cu,
+                            "pratica"=>$rec["pratica"],
+                            "oggetto"=>$rec["oggetto"]
+                        )
+                    );
+
+                }
+                $sezioni=Array();
+                foreach($r as $sez=>$values){
+                    $fogli=Array();
+                    foreach($values as $fgs=>$v){
+                        $mappali=Array();
+                        foreach($v as $maps=>$vals){
+
+                            $pratiche=Array();
+                            foreach($vals as $pr=>$data){
+                                $pratiche[]=$data["info"];
+                                $mappale=$data["mappale"];
+                            }
+                            $mappale=Array("id"=>sprintf("%s-%s-%s",$sez,$fgs,$maps),"name"=>"Mappale $maps","descrizione"=>"","ubicazione"=>"","cu"=>"","children"=>$pratiche,"oggetto"=>"","pratica"=>"","state"=>"closed");
+                            $mappali[]=$mappale;
+                        }
+                        $foglio=Array("id"=>sprintf("%s-%s",$sez,$fgs),"name"=>"Foglio $fgs","descrizione"=>"","ubicazione"=>"","cu"=>"","children"=>$mappali,"oggetto"=>"","pratica"=>"","state"=>"closed");
+                        $fogli[]=$foglio;
+                    }
+                    $sezione=Array("id"=>sprintf("%s",$sez),"name"=>"Sezione $sez","descrizione"=>"","ubicazione"=>"","cu"=>"","children"=>$fogli,"oggetto"=>"","pratica"=>"","state"=>"closed");
+                    $result[]=$sezione;
+                }
+                break;
+        }
+        return $result;
+
     }
 }
 ?>
