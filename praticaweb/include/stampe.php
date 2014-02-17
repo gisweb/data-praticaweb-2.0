b74f2e39c571f0c5c5ddb943da822f23321cbeaa
mamogmx <marco.carbone@gmx.com>
2014-02-17 16:12:03 +0100

    added models

--- praticaweb/include/stampe.php
+++ praticaweb/include/stampe.php
@@ -2,22 +2,23 @@
 //error_reporting(E_ALL);
 $customData["richiedente"]=$customData["richiedenti"];
 $dbh=utils::getDb();
-$sql="SELECT A.pratica,C.* FROM pe.avvioproc A LEFT JOIN pe.vincoli B USING(pratica) LEFT JOIN vincoli.descrizione_vincoli C ON (nome_vincolo=vincolo AND nome_tavola=tavola AND nome_zona=zona)  WHERE pratica=? AND nome_vincolo ILIKE ? and nome_tavola ILIKE ?";
+$sql="SELECT A.pratica,array_to_string(array_agg(C.sigla_zona),', ') as zona FROM pe.avvioproc A LEFT JOIN pe.vincoli B USING(pratica) LEFT JOIN vincoli.descrizione_vincoli C ON (nome_vincolo=vincolo AND nome_tavola=tavola AND nome_zona=zona)  WHERE pratica=? AND nome_vincolo ILIKE ? and nome_tavola ILIKE ? GROUP BY pratica";
 $vincoli=Array(
     // (P.R.I.S.)
-    "vincoli_pris"=>Array($pratica,"PRIS","%"),
+    "zone_piano_1"=>Array($pratica,"PRIS","%"),
     //(PIANI DI BACINO - Fasce di inondabilità)
-    "vincoli_pdb_inondabilita"=>Array($pratica,"PDB","TAV_9"),
+    "zone_piano_2"=>Array($pratica,"PDB","TAV_9"),
     //(PIANI DI BACINO - Suscettività al Dissesto di versante)
-    "vincoli_pdb_dissesto"=>Array($pratica,"PDB","TAV_8"),
+    "zone_piano_8"=>Array($pratica,"PDB","TAV_8"),
     //(P.T.C.P. - Assetto Geomorfologico)
-    "vincoli_ptcpg"=>Array($pratica,"PTCP","ASSETTO_GEOMORFOLOGICO"),
+    "zone_piano_3"=>Array($pratica,"PTCP","ASSETTO_GEOMORFOLOGICO"),
     //(P.T.C.P. - Assetto Insediativo)
-    "vincoli_ptcpi"=>Array($pratica,"PTCP","ASSETTO_INSEDIATIVO"),
+    "zone_piano_4"=>Array($pratica,"PTCP","ASSETTO_INSEDIATIVO"),
     // (P.T.C.P. - Assetto Vegetazionale)
-    "vincoli_ptcpv"=>Array($pratica,"PTCP","ASSETTO_VEGETAZIONALE"),
+    "zone_piano_5"=>Array($pratica,"PTCP","ASSETTO_VEGETAZIONALE"),
     //(P.U.C.)
-    "vincoli_puc"=>Array($pratica,"PUC","%"),
+    "zone_piano_6"=>Array($pratica,"PUC","%"),
+    "zone_piano_7"=>Array($pratica,"PUC","%")
 );
 
 foreach($vincoli as $key=>$params){
@@ -26,7 +27,7 @@ foreach($vincoli as $key=>$params){
          $Errors[$key]=$sth->errorInfo();
     }
     else{
-        $customData[$key]=$sth->fetchAll(PDO::FETCH_ASSOC);
+        $customData[$key]=$sth->fetchColumn(1);
     }
 }
 ?>
