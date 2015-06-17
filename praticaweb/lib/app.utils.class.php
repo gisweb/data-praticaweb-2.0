1809859bf4a88e60ff0d29856f4672422ca3c5d5
mamogmx <marco.carbone@gmx.com>
2015-06-17 12:19:49 +0200

    AB

--- praticaweb/lib/app.utils.class.php
+++ praticaweb/lib/app.utils.class.php
@@ -562,5 +562,21 @@ class appUtils extends generalAppUtils {
                 return Array("totali"=>0,"data"=>Array());
             }
     }
+        static function getNotifiche($userId){
+            $conn=utils::getDb();
+            //DETTAGLI DELLE SCADENZE
+            $lLimit=(defined('LOWER_LIMIT'))?(LOWER_LIMIT):(5);
+            $uLimit=(defined('UPPER_LIMIT'))?(UPPER_LIMIT):(3);
+            $sql="select A.id,A.pratica,B.numero,B.data_prot,testo as oggetto,ARRAY[soggetto_notificato] as interessati from pe.notifiche A inner join pe.avvioproc B using(pratica) where soggetto_notificato=$userId and visionato=0;";
+            
+            $stmt=$conn->prepare($sql);
+            if(!$stmt->execute()){
+                return Array("errore"=>1,"query"=>$sql);
+            }
+            else{
+                $res=$stmt->fetchAll(PDO::FETCH_ASSOC);
+                return Array("totali"=>count($res),"data"=>$res);
+            }
+    }
 }
 ?>
