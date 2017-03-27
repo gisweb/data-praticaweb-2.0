a3e5ec6f47748fe10036c92b8c239d4bbff65d54
mamogmx <marco.carbone@gmx.com>
2017-03-27 10:21:48 +0200

    added layersData

---
+++ praticaweb/js/layersData.js
@@ -0,0 +1,7 @@
+/**
+ * Created by mamo on 27/03/17.
+ */
+var mapLayers = [
+    {"name":"particelle_606","type":"WMS","maxZoom":18,"minZoom":8,"opacity":0.5,"url":"http://camogli.iolweb.it/cgi-bin/mapserv?map=/apps/GisClient-2/mapset/map/camogli_pubblica.map"},
+    {"name":"fabbricati_607","type":"WMS","maxZoom":18,"minZoom":8,"opacity":0.8,"url":"http://camogli.iolweb.it/cgi-bin/mapserv?map=/apps/GisClient-2/mapset/map/camogli_pubblica.map"}
+]
