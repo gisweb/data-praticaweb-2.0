03e0f8a493d76f8847b690ed03a8de79ca413988
mamogmx <marco.carbone@gmx.com>
2017-04-02 12:08:33 +0200

    preview for more application

--- praticaweb/include/cdu.php
+++ praticaweb/include/cdu.php
@@ -25,5 +25,38 @@ if ($stmt->execute(Array($idPratica))){
     }
     $customData["cdu"] = array_values($particella);
 }
-
+$sql= <<<EOT
+WITH A AS(
+SELECT * FROM cdu.mappali order by pratica,coalesce(nullif(regexp_replace(foglio, '[^0-9]+', '', 'g'),''),'0')::integer,coalesce(nullif(regexp_replace(mappale, '[^0-9]+', '', 'g'),''),'0')::integer
+),
+BB AS(
+select A.pratica,Z.protocollo,Z.data,A.foglio,B.descrizione,array_to_string(array_agg(mappale),',') as mappali from cdu.richiesta Z inner join  A USING(pratica) left join vincoli.zona B ON(vincolo=nome_vincolo AND tavola=nome_tavola AND zona=nome_zona) inner join vincoli.vincolo C USING(nome_vincolo) WHERE tavola='INCENDI' AnD pratica= ?group by 1,2,3,4,5 order by pratica,foglio 
+)
+select pratica,descrizione as nome_incendio,array_to_string(array_agg(format('al foglio %s mappali %s,',foglio,mappali)),' ') as particelle from BB group by 1,2;
+EOT;
+$stmt = $dbh->prepare($sql);
+if ($stmt->execute(Array($idPratica))){
+    $res = $stmt->fetchAll();
+    for($i=0;$i<count($res);$i++){
+        $incendi[] = Array("particelle"=>$res[$i]["particelle"],"nome_incendio"=>$res[$i]["particelle"]);
+    }
+    $customData["incendi_cdu"] =$incendi;
+}
+$sql=<<<EOT
+select DISTINCT A.pratica,D.titolo,norma,articolo,format('art_%s.docx',articolo::varchar) as file_name from cdu.richiesta A inner join cdu.mappali B USING(pratica) INNER JOIN cdu.vincoli_norme C USING(vincolo,tavola,zona) INNER JOIN cdu.normativa D ON (id_normativa=C.id) order by pratica,articolo
+EOT;
+$stmt = $dbh->prepare($sql);
+if ($stmt->execute(Array($idPratica))){
+    //require_once APPS_DIR."plugins/openTbs/tbs_class_php5.php";
+    //require_once APPS_DIR."plugins/openTbs/tbs_plugin_opentbs.php";
+   // $TBS = new clsTinyButStrong; // new instance of TBS
+    //$TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN); // load OpenTBS plugin
+    //$TBS->LoadTemplate($this->modelliDir."documentLib".DIRECTORY_SEPARATOR."normativa.docx",OPENTBS_DEFAULT);
+    $res = $stmt->fetchAll();
+    for($i=0;$i<count($res);$i++){
+        $file="";
+        $norme[]=$file;
+    }
+    $customData["normativa"] =$norme;
+}
 ?>
