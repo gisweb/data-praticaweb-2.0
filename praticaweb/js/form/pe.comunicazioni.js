01abc6708a7f93e96087170bb5e7f71924687500
mamogmx <marco.carbone@gmx.com>
2017-07-06 10:34:49 +0200

    added gitginore

--- praticaweb/js/form/pe.comunicazioni.js
+++ praticaweb/js/form/pe.comunicazioni.js
@@ -3,28 +3,39 @@
  */
 $.fn.serializeObject = function() {
     var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name]) {
+    //    var a = this.serializeArray();
+    $(this).find('input[type="hidden"], input[type="text"], input[type="password"], input[type="checkbox"]:checked, input[type="radio"]:checked, select').each(function() {
+        if ($(this).attr('type') == 'hidden') { //if checkbox is checked do not take the hidden field
+            var $parent = $(this).parent();
+            var $chb = $parent.find('input[type="checkbox"][name="' + this.name.replace(/\[/g, '\[').replace(/\]/g, '\]') + '"]');
+            if ($chb != null) {
+                if ($chb.prop('checked')) return;
+            }
+        }
+        if (this.name === null || this.name === undefined || this.name === '')
+            return;
+        var elemValue = null;
+        if ($(this).is('select'))
+            elemValue = $(this).val();
+        else elemValue = $(this).val();
+        if (o[this.name] !== undefined) {
             if (!o[this.name].push) {
                 o[this.name] = [o[this.name]];
             }
-            o[this.name].push(this.value || '');
+            o[this.name].push(elemValue || '');
         } else {
-            o[this.name] = this.value || '';
+            o[this.name] = elemValue || '';
         }
     });
     return o;
-};
-
+}
 
 $(document).ready(function(){
     $("#richiesta_prot").button({
         icons:{primary:'ui-icon-work'}
     }).bind('click',function(event){
         event.preventDefault();
-        var formData = $("form #comunicazioni").serializeObject();
-
+        var formData = $("#comunicazioni").serializeObject();
         $.ajax({
             url:"/services/local/xServer.php",
             type:'POST',
--- praticaweb/js/form/ricerca.js
+++ praticaweb/js/form/ricerca.js
@@ -1,3 +1,4 @@
+var pippo = "";
 var result={};
 var dataPost={};
 $(document).ready(function(){
