f6f14da57bae4d9cb5dbe585d1b82665b69363cb
mamogmx <marco.carbone@gmx.com>
2017-07-06 10:05:52 +0200

    get serialize object

--- praticaweb/js/form/pe.comunicazioni.js
+++ praticaweb/js/form/pe.comunicazioni.js
@@ -1,6 +1,23 @@
 /**
  * Created by mamo on 05/07/17.
  */
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+
 $(document).ready(function(){
     $("#richiesta_prot").button({
         icons:{primary:'ui-icon-work'}
