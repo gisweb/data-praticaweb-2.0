abf81b487b18f4823e657617f9b1b9f47abcade6
mamogmx <marco.carbone@gmx.com>
2017-07-07 07:36:08 +0200

    protocollo sicraweb devel

--- praticaweb/lib/protocollo.class.php
+++ praticaweb/lib/protocollo.class.php
@@ -5,8 +5,26 @@
  * Date: 06/07/17
  * Time: 17:55
  */
+
+require_once LIB."nusoap".DIRECTORY_SEPARATOR."nusoap.php";
+require_once LIB."nusoap".DIRECTORY_SEPARATOR."nusoapmime.php";
 class protocollo{
-    static function richiediProt(){
+    static function richiediProtOut($params=Array()){
+
+    }
+
+    static function richiediProtIn(){
+
+    }
+
+    static function infoProtocollo(){
+
+    }
+
+    private static function inserisciDocumento($id){
+
+    }
+    private function recuperaDocumento(){
 
     }
 }
