7db950b35d3b9e1c36badde83df580c387b291cf
mamogmx <marco.carbone@gmx.com>
2017-07-11 17:02:56 +0200

    working on prot

--- protocollo.config.php
+++ protocollo.config.php
@@ -8,27 +8,20 @@
 
 define('TEMPLATE_DIR',DATA_DIR."praticaweb".DIRECTORY_SEPARATOR."templates".DIRECTORY_SEPARATOR);
 
-$paramsProt = Array(
+$paramsProtOut = Array(
     "service"=>"SicraWeb",
     "wsUrl"=> "http://93.57.10.175:50080/client/services/ProWSApi?WSDL",
     "login"=> "!gisweb/cec.gisweb@sicraweb/sicraweb",
-    "destinatario"=> Array(
+    "mittente"=> Array(
+        "Denominazione_Entita"=> "Comune di Andora",
         "Denominazione"=>"URBANISTICA",
         "CodiceAmministrazione"=>"c_l315",
         "IndirizzoTelematico"=>"comune@prova.it",
         "UnitaOrganizzativa"=>"URB",
         "CodiceTitolario"=>"1.1",
-        "CodiceA00"=>"udcvem"
-    ),
-    "mittente"=> Array(
-        "Nome"=>"fisica_nome",
-        "Cognome"=>"fisica_cognome",
-        "CodiceFiscale"=>"fisica_cf",
-        "IndirizzoTelematico"=>"fisica_email",
-        "Indirizzo"=>"fisica_indirizzo",
-        "CAP"=>"fisica_cap",
-        "Comune"=>"fisica_comune",
-        "Prov"=>"fisica_provincia"
+        "CodiceA00"=>"udcvem",
+        "Indirizzo" => "Via Cavour 94",
+        "Identificativo" => ""
     )
 );
 
