a0d5aa2dfe48c705a8fb90d2ab762729309d57b2
mamogmx <marco.carbone@gmx.com>
2017-07-11 10:28:33 +0200

    working on prot

--- test.php
+++ test.php
@@ -12,7 +12,12 @@ define('APPS_DIR',"/apps/PraticaWeb-2.1-dev/");
 require_once "config.php";
 require_once "protocollo.config.php";
 require_once LOCAL_LIB."protocollo.class.php";
+echo "A";
+$r = protocollo::recuperaSoggetto("27675");
+if ($r["success"]==1){
+    $sogg = $r["result"];
+    $r = protocollo::caricaXML("destinatario",$sogg);
+    echo "<pre>";print_r($r);echo "</pre>";
+}
 
-$sogg = protocollo::recuperaSoggetto("27675");
-echo "<pre>";print_r($sogg);echo "</pre>";
 ?>
