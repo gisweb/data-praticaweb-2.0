2ab09dbfb1644f81c553da7acb6bb53352632c43
mamogmx <marco.carbone@gmx.com>
2017-07-11 11:19:33 +0200

    working on prot

--- test.php
+++ test.php
@@ -5,20 +5,26 @@
  * Date: 11/07/17
  * Time: 09:17
  */
-
+error_reporting(E_ERROR);
 $dir = dirname(__FILE__);
 define('DATA_DIR',$dir.DIRECTORY_SEPARATOR);
 define('APPS_DIR',"/apps/PraticaWeb-2.1-dev/");
 require_once "config.php";
 require_once "protocollo.config.php";
 require_once LOCAL_LIB."protocollo.class.php";
-echo "A";
-$r = protocollo::recuperaSoggetto("27675");
-if ($r["success"]==1){
-    $sogg = $r["result"];
-    $sogg["modalita_invio"] = "PEC";
-    $r = protocollo::caricaXML("destinatario",$sogg);
-    echo "<pre>";print_r($r);echo "</pre>";
+$s = Array("27675","27674","autostrade@pec.it");
+for($i=0;$i<count($s);$i++){
+    $id = $s[$i];
+    $r = protocollo::recuperaSoggetto($id);
+    if ($r["success"]==1){
+        $sogg = $r["result"];
+        $sogg["modalita_invio"] = "PEC";
+        $r[] = protocollo::caricaXML("destinatario_multi",$sogg);
+        $soggetti[] = $sogg["denominazione"];
+    }
 }
+$data=Array("destinatari"=>implode(", ",$soggetti),"destinatario_multi"=>implode("\n",$r));
+$r = protocollo::caricaXML("destinatari",$data);
 
+echo "<pre>";print_r($r);echo "</pre>";
 ?>
