d38577d75db7cedca28ac3974b0a92b8dcf15829
mamogmx <marco.carbone@gmx.com>
2017-07-21 15:46:37 +0200

    A

--- praticaweb/db/db.pe.comunicazioni.php
+++ praticaweb/db/db.pe.comunicazioni.php
@@ -2,19 +2,24 @@
 $idpratica=$_REQUEST["pratica"];
 $action = $_REQUEST["azione"];
 
-if (in_array($action,Array("Salva","Elimina"))){
-	
+if (in_array($action,Array("Salva","Elimina","Protocolla"))){
+	if ($_REQUEST["azione"]=="Protocolla") $_REQUEST["azione"]='Salva';
+        include_once "./db/db.savedata.php";
+        $id = ($_REQUEST["id"])?($_REQUEST["id"]):($_SESSION["ADD_NEW"]);
 	if ($action=="Protocolla" && defined('PROT_OUT')  && PROT_OUT==1){
-        require_once LOCAL_LIB."wsProtocollo.class.php";
-        $prot = new wsProtocollo('U','PEC');
-        $params["id"] = 1;
-        $params["allegati"] = Array(1393,1392);
-        $params["destinatari"] = Array(27675,27674);
-        $params["app"] = "pe";
-        $r = $prot->richiediProtOut($idpratica,$params);
-
+            require_once LOCAL_LIB."wsProtocollo.class.php";
+            $allegati = $_POST["allegati"];
+            $destinatari = $_POST["destinatari"]
+            $prot = new wsProtocollo('U','PEC');
+            $params["id"] = $id;
+            $params["allegati"] = $allegati;
+            $params["destinatari"] = $destinatari;
+            $params["app"] = "pe";
+            $r = $prot->richiediProtOut($idpratica,$params);
+            print_array($r);
+            $_REQUEST["mode"]="edit";
+            include_once "./db/db.savedata.php";
 	}
-	include_once "./db/db.savedata.php";
 }
 
 	
