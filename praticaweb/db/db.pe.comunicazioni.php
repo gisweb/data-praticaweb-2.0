e0631ea52cf35f2ad28f7b4cdb15594d13c3c734
mamogmx <marco.carbone@gmx.com>
2017-07-21 16:24:31 +0200

    working on prot dev

--- praticaweb/db/db.pe.comunicazioni.php
+++ praticaweb/db/db.pe.comunicazioni.php
@@ -4,7 +4,7 @@ $action = $_REQUEST["azione"];
 
 if (in_array($action,Array("Salva","Elimina","Protocolla"))){
 	if ($_REQUEST["azione"]=="Protocolla") $_REQUEST["azione"]='Salva';
-        include_once "./db/db.savedata.php";
+        include_once APPS_DIR."/db/db.savedata.php";
         $id = ($_REQUEST["id"])?($_REQUEST["id"]):($_SESSION["ADD_NEW"]);
 	if ($action=="Protocolla" && defined('PROT_OUT')  && PROT_OUT==1){
             require_once LOCAL_LIB."wsProtocollo.class.php";
@@ -18,7 +18,7 @@ if (in_array($action,Array("Salva","Elimina","Protocolla"))){
             $r = $prot->richiediProtOut($idpratica,$params);
             print_array($r);
             $_REQUEST["mode"]="edit";
-            include_once "./db/db.savedata.php";
+            include_once APPS_DIR."./db/db.savedata.php";
 	}
 }
 
