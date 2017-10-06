66059a3ac260565d7a21ba26929c8e5f67e39446
mamogmx <marco.carbone@gmx.com>
2017-10-06 08:35:22 +0200

    A

---
+++ praticaweb/lib/pratica.class.php
@@ -0,0 +1,8 @@
+<?php
+use Doctrine\Common\ClassLoader;
+require_once APPS_DIR.'plugins/Doctrine/Common/ClassLoader.php';
+require_once APPS_DIR.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'pratica.class.php';
+class pratica extends generalPratica{
+    
+}
+?>
