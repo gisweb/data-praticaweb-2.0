90187e5bbffed7c224ab8ff7ffb903c43061d37b
mamogmx <marco.carbone@gmx.com>
2017-10-20 11:06:36 +0200

    Added layerData.js

--- praticaweb/js/form/layerData.js
+++
@@ -1,4 +0,0 @@
-var mapLayers = [
-    {"name":"particelle_606","type":"WMS","maxZoom":18,"minZoom":8,"opacity":0.5,"url":"http://andora.praticaweb.it/cgi-bin/mapserv?map=/apps/GisClient-2/mapset/map/andora_pubblica.map"},
-    {"name":"fabbricati_607","type":"WMS","maxZoom":18,"minZoom":8,"opacity":0.8,"url":"http://andora.praticaweb.it/cgi-bin/mapserv?map=/apps/GisClient-2/mapset/map/andora_pubblica.map"}
-]
---
+++ praticaweb/js/layerData.js
@@ -0,0 +1,4 @@
+var mapLayers = [
+    {"name":"particelle_606","type":"WMS","maxZoom":18,"minZoom":8,"opacity":0.5,"url":"http://sanremo.praticaweb.it/cgi-bin/mapserv?map=/apps/GisClient-2/mapset/map/sanremo_riservata.map"},
+    {"name":"fabbricati_607","type":"WMS","maxZoom":18,"minZoom":8,"opacity":0.8,"url":"http://sanremo.praticaweb.it/cgi-bin/mapserv?map=/apps/GisClient-2/mapset/map/sanremo_riservata.map"}
+]
