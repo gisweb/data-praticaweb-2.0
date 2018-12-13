fa802288fbfac4a9014b1994c167b89e14ca7cb8
mamogmx <root@vm-svsit.comunesavona.private>
2018-12-13 16:51:06 +0100

    BB

---
+++ config.ads.php
@@ -0,0 +1,14 @@
+<?php
+
+define('WSDL_LOGIN_URL',"http://documentale.comune.savona.it/JProtocolloDocArea/services/DOCAREAProtoSoap?WSDL");
+define('WSDL_SFERA_URL',"http://documentale.comune.savona.it/Atti/services/atti?wsdl");
+define('WSDL_PROTEXT_URL',"http://documentale.comune.savona.it/JProtocolloDocArea/services/ProtocolloExtendedServicePort?wsdl");
+define('WSDL_DOCUM_URL',"http://documentale.comune.savona.it/Protocollo/services/protocollo?wsdl");
+
+define('SERVICE_USER','AGSPRWS');
+define('SERVICE_PASSWD','password');
+
+define("CODICE_A00","AOOCOMUNESV");
+define("CODICE_ENTE","C_I480");
+
+?>
---
+++ praticaweb/lib/ads.ws.class.php
@@ -0,0 +1,24 @@
+<?php
+$confFile = sprintf("%s%sconfig.ads.php",dirname(dirname(dirname(__FILE__))),DIRECTORY_SEPARATOR);
+require_once $confFile;
+class adsWS{
+	var $wsLogin = WSDL_LOGIN_URL;
+	var $wsExtProt = WSDL_PROTEXT_URL;
+	var $wsDocum = WSDL_DOCUM_URL;
+	var $wsSfera = WSDL_SFERA_URL;
+	
+	var $DST;
+	var $user;
+	var $passwd;
+	var $codEnte;
+	
+	var $xmlTemplate;
+	
+	function __construct($usr,$pwd,$codEnte){
+		$this->$user = $user;
+		$this->passwd = $pwd;
+		$this->codEnte = $codEnte;
+	}
+}
+
+?>
