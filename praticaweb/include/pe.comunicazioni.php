c975411b96a9e097c57accae78ffecea82250de4
mamogmx <marco.carbone@gmx.com>
2019-07-24 15:48:44 +0200

    Aggiunto WS per verifica INVIO nei dati

--- praticaweb/include/pe.comunicazioni.php
+++ praticaweb/include/pe.comunicazioni.php
@@ -1,9 +1,37 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-print_array($arrayData);
+require_once DATA_DIR."config.protocollo.php";
+require_once LOCAL_LIB."wsProtocollo.class.php";
+$dbh = utils::getDB();
+$ws = new wsMail();
+for($i=0;$i<count($arrayData);$i++){
+    $d = $arrayData[$i];
+    if($d["protocollo"] && $d["id_comunicazione"]){
+        $anno = date('Y',strtotime(str_replace('/','-',$d["data_protocollo"])));
+        $res = $ws->verificaInvio($d["protocollo"], $anno, $d["id_comunicazione"]);
+        if ($res["success"]===1){
+            $tmp = Array();
+            for($j=0;$j<count($res["accettazione"]);$j++){
+                $r = $res["accettazione"][$j];
+                $tmp[] = sprintf("Comunicazione accettata il %s con id %s",$r["dataAccetazione"],$r["idRepAccetazione"]);
+            }
+            $d["accettazione"] = implode("\n",$tmp);
+            $tmp = Array();
+            for($j=0;$j<count($res["consegna"]);$j++){
+                $r = $res["consegna"][$j];
+                $tmp[] = sprintf("Comunicazione consegnata il %s con id %s a %s",$r["dataAccetazione"],$r["idRepAccetazione"],$r["emailDestinatario"]);
+            }
+            $d["consegna"] = implode("\n",$tmp);
+        }
+        else{
+            $d["accettazione"]=" --- ";
+            $d["consegna"] = " --- ";
+        }
+    }
+    else{
+        $d["accettazione"]=" --- ";
+        $d["consegna"] = " --- ";
+        $d["data_invio"] = " --- ";
+    }
+    $arrayData[$i] = $d;
+}
 ?>
