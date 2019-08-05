72c86d49096654c0f2a2c945782950614c85cde8
mamogmx <marco.carbone@gmx.com>
2019-08-05 13:26:06 +0200

    Protocollazione in uscita login Ok Test searchFascicoli

--- praticaweb/lib/wsprotocollo.class.php
+++ praticaweb/lib/wsprotocollo.class.php
@@ -8,6 +8,30 @@ class protocollo{
     function login(){
         $cl = new SoapClient(SERVICE_URL,array("trace" => 1, "exception" => 0));
         $res = $cl->Login(Array("strCodEnte"=>CODICE_AMMINISTRAZIONE,"strUserName"=>SERVICE_USER,"strPassword"=>SERVICE_PASSWD));
+        $res = json_decode(json_encode($res),TRUE);
+        
+        if(array_key_exists("LoginResult",$res)){
+            if(array_key_exists("lngErrNumber", $res["LoginResult"]) && !$res["LoginResult"]["lngErrNumber"]){
+                return Array("success"=>1,"dst"=>$res["LoginResult"]["strDST"]);
+            }
+            else{
+                return Array("success"=>0,"message"=>$res["LoginResult"]["strErrString"]);
+            }
+        }
+        else{
+            return Array("success"=>-1);
+        }
+        unset($cl);
+    }
+    
+    function cercaFascicolo($prot,$anno){
+        $res = $this->login();
+        if ($res["success"]===1){
+            $dst = $res["dst"];
+            $cl = new SoapClient(DIZIONARI_URL,array("trace" => 1, "exception" => 0));
+            $result = $cl->searchFascicoli(Array("strUserName"=>SERVICE_USER,"strDST"=>$dst,"codiceAOO"=>Codice_A00,"numeroProtocollo"=>$prot,"annoProtocollo"=>$anno));
+            return $result;
+        }
     }
 }
 ?>
