5f56a55bac362fcf5257e28857e009e7f06daac8
mamogmx <marco.carbone@gmx.com>
2019-09-10 12:13:10 +0200

    QUERY CDU FINITA

--- praticaweb/include/cdu.php
+++ praticaweb/include/cdu.php
@@ -17,8 +17,22 @@ FROM
 cdu.mappali D INNER JOIN vincoli.vincolo A ON(vincolo=A.nome_vincolo)
 INNER JOIN vincoli.tavola B on (vincolo=B.nome_vincolo AND tavola = B.nome_tavola) 
 INNER JOIN vincoli.zona C on(vincolo=C.nome_vincolo AND tavola=C.nome_tavola AND zona=nome_zona) 
-where cdu=1 and pratica=9 order by foglio,mappale
+where cdu=1 and pratica=? order by foglio,mappale
 EOT;
-
+$dbh = utils::getDb();
+$stmt = $dbh->prepare($sql);
+if(!$stmt->execute(Array($idPratica))){
+    
+}
+else{
+    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    for($i=0;$i<count($res);$i++){
+        $d = $res[$i];
+        $key = sprintf("%s-%s",$d["foglio"],$d["mappale"]);
+        $mappali[$key]["foglio"]=$d["foglio"];
+        $mappali[$key]["mappale"]=$d["mappale"];
+        $mappali[$key]["vincoli"][]=$d;
+    }
+}
 $customData["mappali"]=$mappali;	
 ?>
